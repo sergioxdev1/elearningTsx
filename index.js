0045e2560e57c98028b8e80942bcb689c2d4550e
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-01-22 14:19:50 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+// visual sectin - Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>E-Learning Platform</h1> <p>E-Learning Platform</p>`;
+
+//Excersices
+
+//Add numbers only qith operators
+
+function addNumber(a,b) {
+  return (a>0 || b>0) ? a+b : "negative number"
+  }
+  
+  console.log(addNumber(10,-2))
