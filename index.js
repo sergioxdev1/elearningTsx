cc88abd7b33079c10378097e2823e9a1a1b12367
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-02-01 18:49:08 -0700

    1 file modified

--- index.js
+++ index.js
@@ -293,3 +293,22 @@ zeus.takeOff();
 const atlas = new Rocket();
 // prints To the moon! in console
 atlas.launch();
+
+
+//GETTERS AND SETTERS
+class Thermostat{
+  constructor (fahrenheit){
+    this._fahrenheit = fahrenheit;
+  }
+  get temperature(){
+    return (5 / 9) * (this._fahrenheit - 32);
+  }
+  set temperature(celsius){
+    this._fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
