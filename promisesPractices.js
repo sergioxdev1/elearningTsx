f4c28d836532971bea75cc227114431cca2b5b17
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-02-06 22:38:20 -0700

    1 file modified

--- promisesPractices.js
+++ promisesPractices.js
@@ -7,9 +7,11 @@ Make the promise handle success and failure. If responseFromServer is true, call
 const makeServerRequest = new Promise((resolve, reject) => {
   // responseFromServer represents a response from a server
   let responseFromServer;
-    
-  responseFromServer? resolve("We got the data") :  reject("Data not received");
+
+  responseFromServer ? resolve('We got the data') : reject('Data not received');
 });
+
+console.log(responseFromServer.resolve);
 
 /*Manejar una promesa cumplida (FULLFILED PROMISE) con entonces
 Las promesas son más útiles cuando tiene un proceso que toma una cantidad de tiempo desconocida en su código (es decir, algo asíncrono), a menudo una solicitud del servidor. Cuando realiza una solicitud del servidor, lleva algo de tiempo y, una vez que se completa, generalmente desea hacer algo con la respuesta del servidor. Esto se puede lograr usando el método entonces. El método then se ejecuta inmediatamente después de que se cumpla su promesa con resolve. Aquí hay un ejemplo:*/
