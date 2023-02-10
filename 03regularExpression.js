1e50cc6b7edfa4ab81aeb2c8fc81dd162d58b50a
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-02-10 15:05:05 -0700

    1 file modified

--- 03regularExpression.js
+++ 03regularExpression.js
@@ -1,6 +1,7 @@
 //Encontrar caracteres en cadenas por medio de experesiones regulares
 //METODO TEST()
+export function ejercicios01(){
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
-console.log(result);
+console.log(result);}
