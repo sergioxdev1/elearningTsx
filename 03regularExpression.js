a9dba643a4fd5921abb486fa4da78788cec22527
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-02-10 15:08:59 -0700

    1 file modified

--- 03regularExpression.js
+++ 03regularExpression.js
@@ -4,4 +4,9 @@ export default function ejercicios01to05(){
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
-console.log(result);}
+console.log("result: "+result);}
+
+let petString = "James has a pet cat.";
+let petRegex = /cat|dog|bird|fish/; // Cambia esta línea
+let result2 = petRegex.test(petString);
+console.log("result2: "+result2);
