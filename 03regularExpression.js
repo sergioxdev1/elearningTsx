cc754534cccdeb8dda6d820359493acdf81652dc
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-02-10 15:13:09 -0700

    1 file modified

--- 03regularExpression.js
+++ 03regularExpression.js
@@ -10,3 +10,12 @@ let petString = "James has a pet cat.";
 let petRegex = /cat|dog|bird|fish/; // Cambia esta línea
 let result2 = petRegex.test(petString);
 console.log("result2: "+result2);
+
+let myString2 = "freeCodeCamp";
+let fccRegex = /FREECODECAMP/; // Sin el flag "i" para ignorar case sensitive
+let result3 = fccRegex.test(myString2);
+console.log("result3: "+result3);
+
+fccRegex = /FREECODECAMP/i;// Con el flag "i" 
+result3 = fccRegex.test(myString2);
+console.log("result3: "+result3);
