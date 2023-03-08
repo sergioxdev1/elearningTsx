0c98ddcaf603e6ad4e6de68ad93740691198aa93
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-03-07 22:48:11 -0700

    2 files modified

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ appDiv.innerHTML = `
 First, you'll build a cat photo app to learn the basics of HTML and CSS. Later, you'll learn modern techniques like CSS variables by building a penguin, and best practices for accessibility by building a quiz site.</p>
 <p>
 Finally, you'll learn how to make webpages that respond to different screen sizes by building a photo gallery with Flexbox, and a magazine article layout with CSS Grid.</p>
+
+<a href="/HTML-Mod01.html">Learn HTML by Building a Cat Photo App</a>
+<button>Learn Basic CSS by Building a Cafe Menu</button>
 </div>
 `;
